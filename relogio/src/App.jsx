4641fb48463fd3129d12b3e5e89b35357976bafc
pedroopon <xppontes@gmail.com>
2024-05-21 23:15:12 -0300

    first commit

---
+++ relogio/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import RelogioDigital from './components/RelogioDigital'
+
+function App() {
+
+  return (
+    <div >
+      <RelogioDigital/>
+    </div>
+  )
+}
+
+export default App
---
+++ relogio/src/components/RelogioDigital.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react'; 
+import './RelogioDigital.css'
+
+  
+
+function RelogioDigital() { 
+
+  const [hora, setHora] = useState(new Date().toLocaleTimeString()); 
+
+  
+
+  useEffect(() => { 
+
+    const interval = setInterval(() => { 
+
+      setHora(new Date().toLocaleTimeString()); 
+
+    }, 1000); 
+
+  
+
+    return () => clearInterval(interval); 
+
+  }, []); 
+
+  
+
+  return ( 
+
+    <div className="container"> 
+
+      <h2>Relógio Digital</h2> 
+
+      <p>{hora}</p> 
+
+    </div> 
+
+  ); 
+
+} 
+
+  
+
+export default RelogioDigital; 
